31fac0f3ff9a67bb9233053b7644a0878a2c3a7e
AndinhoSKT <andinhoalderigi.1998@hotmail.com>
2024-10-22 22:45:05 -0300

    UsiarioController

---
+++ MinhaAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MinhaAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet("ObterDataHoraAtual")]
+        public IActionResult ObterDataHora() 
+        {
+            var obj = new 
+            {
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToShortTimeString(),
+            };
+
+            return Ok(obj);
+        }
+        
+    }
+}
