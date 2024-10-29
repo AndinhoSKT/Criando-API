f61ad13babcfb51f2af80c87f4cba6375900e162
AndinhoSKT <andinhoalderigi.1998@hotmail.com>
2024-10-28 22:22:27 -0300

    Criando controllers e usando entity com o Banco de dados

---
+++ MinhaAPI/Controllers/ContatoController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using MinhaAPI.Context;
+using MinhaAPI.Entities;
+
+namespace MinhaAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContatoController : ControllerBase
+    {
+        private readonly AgendaContext _context;
+
+        public ContatoController(AgendaContext context)
+        {
+            _context = context;
+        } 
+
+        [HttpPost]
+        public IActionResult Create(Contato contato)
+        {
+            _context.Add(contato);
+            _context.SaveChanges();
+            return Ok(contato);
+        }
+    }
+}
